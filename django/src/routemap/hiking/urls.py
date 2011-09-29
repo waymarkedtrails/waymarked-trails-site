459548345b4429f6fd4a26058a56bdfb9001cf17
Sarah Hoffmann <lonvia@denofr.de>
2011-09-29 23:48:07 +0200

    adapt hiking helppage stuff to new schema

--- django/src/routemap/hiking/urls.py
+++ django/src/routemap/hiking/urls.py
@@ -35,8 +35,7 @@ helppageinfo = {
 
 urlpatterns += patterns('routemap.views.helppages',
     ('osmc_symbol_legende', 'osmc_symbol_legende'),
-    (r'^(?P<page>[\w/]+)$', 'helppage_view', 
-              {'source' : 'helppages/hiking_about'}, 'helppage'),
+    (r'^(?P<page>[\w/]+)$', 'helppage_view', helppageinfo, 'helppage'),
         
 )
 

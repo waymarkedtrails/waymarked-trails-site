ffb3fe1f68e1e80c7b52a68b9553edf6321c0ab6
Sarah Hoffmann <lonvia@denofr.de>
2012-12-09 11:15:08 +0100

    local config has moved
    Remove example again. Can't really gitignore commited files.

--- config/siteconfig_local.py
+++
@@ -1,9 +0,0 @@
-# Use this file to change local configuration settings.
-#
-# For available settings and the correct format, see config/siteconfig.py
-
-# As a minimum, you must state a contact address or search will not work.
-#ADMINS = (
-#    ('Me', 'admin@mysite.org'),
-#)
-

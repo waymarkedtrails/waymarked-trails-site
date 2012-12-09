20c9f8c2627a68529d7b175967e0c89db0955110
Sarah Hoffmann <lonvia@denofr.de>
2012-12-09 11:05:43 +0100

    add an example siteconfig_local

---
+++ config/siteconfig_local.py
@@ -0,0 +1,9 @@
+# Use this file to change local configuration settings.
+#
+# For available settings and the correct format, see config/siteconfig.py
+
+# As a minimum, you must state a contact address or search will not work.
+#ADMINS = (
+#    ('Me', 'admin@mysite.org'),
+#)
+

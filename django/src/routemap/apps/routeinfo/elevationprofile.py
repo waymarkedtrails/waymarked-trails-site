883079d9196e415887921916d28bec4f25a7c1ba
Sarah Hoffmann <lonvia@denofr.de>
2013-01-20 17:53:56 +0100

    add copyright header

--- django/src/routemap/apps/routeinfo/elevationprofile.py
+++ django/src/routemap/apps/routeinfo/elevationprofile.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
+# This file is part of the Waymarked Trails Map Project
+# Copyright (C) 2012-2013 Espen Oldeman Lund
+#
+# This is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 
 from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponseNotFound
 from django.conf import settings
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
-from django.views.generic.simple import direct_to_template
 
 import django.contrib.gis.geos as geos
 

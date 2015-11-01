db9dae3f30f7c654a2a66c90fdb01031284b668a
Sarah Hoffmann <lonvia@denofr.de>
2015-11-01 23:00:46 +0100

    add database access for api

--- api/routes.py
+++ api/routes.py
@@ -20,21 +20,104 @@ from os.path import join as os_join
 from os import environ as os_environ
 import cherrypy
 from datetime import datetime
+import sqlalchemy as sa
 
 import config.defaults
 
+class Bbox(object):
+
+    def __init__(self, value):
+        parts = value.split(',')
+        if len(parts) != 4:
+            raise cherrypy.HTTPError(400, "No valid map area specified. Check the bbox parameter in the URL.")
+        try:
+            self.coords = tuple([float(x) for x in parts])
+        except ValueError:
+            raise cherrypy.HTTPError(400, "Invalid coordinates given for the map area. Check the bbox parameter in the URL.")
+
+@cherrypy.tools.db()
+@cherrypy.tools.add_language()
 class RoutesApi(object):
 
     def __init__(self, confname, config):
         self.config = config
-        self.basename = confname
+        # sub-directories
+        self.list = ListRoutes()
+        self.relation = RelationInfo()
 
     @cherrypy.expose
     def index(self):
-        return "Hello API (%s)" % self.basename
+        return "Hello API"
 
     @cherrypy.expose
     def last_update(self):
         return datetime.now().isoformat(' ')
 
+
+@cherrypy.tools.json_out()
+class ListRoutes(object):
+
+    @cherrypy.expose
+    def by_area(self, bbox, **params):
+        b = Bbox(bbox)
+
+        if 'lang' in params:
+            lang = (params)
+        else:
+            lang = cherrypy.request.lang_list
+
+        mapdb = cherrypy.request.app.config['DB']['map']
+        conn = cherrypy.request.db
+
+        r = mapdb.tables.routes.data
+        res = sa.select([r.c.id, r.c.name])\
+               .where(r.c.id == 29003)
+
+        out = []
+        for r in conn.execute(res):
+            out.append({ 'id' : r['id'],
+                         'name' : r['name']
+                       })
+
+        return out
+        #   qs = getattr(table_module, table_class).objects.filter(top=True).extra(where=(("""
+        #    id = ANY(SELECT DISTINCT h.parent
+        #             FROM hierarchy h,
+        #                  (SELECT DISTINCT unnest(rels) as rel
+        #                   FROM segments
+        #                   WHERE geom && st_transform(ST_SetSRID(
+        #                     'BOX3D(%f %f, %f %f)'::Box3d,4326),%%s)) as r
+        #             WHERE h.child = r.rel)""" 
+        #    % coords) % settings.DATABASES['default']['SRID'],)).order_by('level')
+
+
+    @cherrypy.expose
+    @cherrypy.popargs('zoom', 'x', 'y')
+    def tile(self, zoom, x, y):
+        return "TODO: get data for tile %s/%s/%s" % (zoom, x, y)
+
+    def search(self, query=None):
+        return "TODO: search"
+
+    def segments(self, ids=None, bbox=None):
+        return "TODO: jsonbox"
+
+@cherrypy.popargs('oid')
+class RelationInfo(object):
+
+    @cherrypy.expose
+    def index(self, oid):
+        return "TODO: General info about relation %s" % oid
+
+    @cherrypy.expose
+    def geom(self, oid):
+        return "TODO: geometry of relation %s" % oid
+
+    @cherrypy.expose
+    def wikilink(self, oid):
+        return "TODO: wikilink of relation %s" % oid
+
+    @cherrypy.expose
+    def gpx(self, oid):
+        return "TODO: GPX of relation %s" % oid
 

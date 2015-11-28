50e31a7d00032259ef7c0b65e0ba20b9b773b02e
Sarah Hoffmann <lonvia@denofr.de>
2015-11-28 22:38:29 +0100

    move map js in its own file

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -78,65 +78,6 @@ Osgende.FormFill = {
 
 }
 
-Osgende.BaseMapControl = function() {
-  var obj = {};
-
-  function map_move_end(evt) {
-    var view = evt.map.getView();
-    var zoom = view.getZoom()
-    var center = ol.proj.transform(view.getCenter(), "EPSG:3857", "EPSG:4326");
-    var map_param = "map=" + zoom + '!' +
-                    (Math.round(center[1] * 10000) / 10000) + '!' +
-                    (Math.round(center[0] * 10000) / 10000);
-
-    var h = window.location.hash || '#';
-    if (h.indexOf('?') < 0)
-        h = h + '?' + map_param;
-    else if (h.indexOf('map=') >= 0)
-        h = h.replace(new RegExp("map=[^&]*"), map_param);
-    else
-        h = h + '&' + map_param;
-
-    window.history.replaceState(window.history.state, document.title, h);
-
-    if (Modernizr.localstorage) {
-      localStorage.setItem('location',
-                           JSON.stringify({ center: center, zoom: zoom}));
-    }
-  }
-
-  var init_view = { center: [8.6517, 46.6447], zoom: 11 };
-  if (Modernizr.localstorage && localStorage.getItem('location') !== null) {
-    init_view = JSON.parse(localStorage.getItem('location'));
-  }
-
-  var url_view = decodeURI(window.location.hash.replace(
-               new RegExp("^(?:.*[&\\?]map(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-  if (url_view) {
-    var parts = url_view.split('!');
-    if (parts.length === 3) {
-      init_view = { zoom : parseInt(parts[0], 10),
-                    center : [parseFloat(parts[2]), parseFloat(parts[1])] };
-    }
-  }
-
-  obj.map = new ol.Map({
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    controls: ol.control.defaults({ attribution: false }),
-    target: 'map',
-    view: new ol.View({ center: ol.proj.transform(init_view.center, "EPSG:4326", "EPSG:3857"),
-                      zoom: init_view.zoom })
-  });
-
-  obj.map.on('moveend', map_move_end);
-
-  return obj;
-}
-
 Osgende.RouteList = function(map, container) {
   var obj = {};
 

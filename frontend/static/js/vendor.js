c77fe1085c1e3f10021d3a45a3886ab5060e1277
nebulon42 <nebulon42@mailbox.org>
2017-07-15 23:14:10 +0200

    manage external JS dependencies wih NPM, move to Sass/SCSS, use Webpack to bundle external JS, use Gulp as task runner for compiling CSS and bundling JS

---
+++ frontend/static/js/vendor.js
@@ -0,0 +1,8 @@
+window.jQuery = window.$ = require('jquery').noConflict();
+
+require('browsernizr/test/storage/localstorage');
+window.Modernizr = require('browsernizr');
+
+window.ol = require('openlayers');
+
+require('flot-latest');
